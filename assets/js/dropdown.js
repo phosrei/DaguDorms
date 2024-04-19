b389d6d7fa14ba682b527871c82d4caf6747616b
Reiki <phosreiki@gmail.com>
2024-04-20 05:42:09 +0800

    Fix responsiveness for checking

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,6 +1,5 @@
 function toggleDropdown() {
     var dropdownMenu = document.getElementById("dropdown-menu");
-    var overlay = document.getElementById("overlay");
 
     dropdownMenu.classList.toggle("open");
 
@@ -11,9 +10,36 @@ function toggleDropdown() {
     }
 }
 
+function toggleFilters() {
+    var filtersContainer = document.getElementById("filters-container");
+
+    filtersContainer.classList.toggle("open");
+
+    if (filtersContainer.classList.contains("open")) {
+        overlay.style.display = "block";
+    } else {
+        overlay.style.display = "none";
+    }
+}
+
+function checkOverlay() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    var filtersContainer = document.getElementById("filters-container");
+
+    if (dropdownMenu) {
+        dropdownMenu.classList.remove("open");
+    }
+
+    if (filtersContainer) {
+        filtersContainer.classList.remove("open");
+    }
+
+    overlay.style.display = "none";
+}
 var closeButton = document.getElementById("close-button");
+var overlay = document.getElementById("overlay");
 closeButton.addEventListener("click", toggleDropdown);
+overlay.addEventListener('click', checkOverlay);
 
 document.getElementById('dropdown-button').addEventListener('click', toggleDropdown);
-
-overlay.addEventListener('click', toggleDropdown);
+document.getElementById('filters-button').addEventListener('click', toggleFilters);
